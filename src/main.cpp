a407598a6f89adf572b0f447bb52e12f292e5a84
kofta <duakickflip@gmail.com>
2024-06-21 19:45:44 +0300

    change code styling

--- src/main.cpp
+++ src/main.cpp
@@ -4,62 +4,57 @@
 #include "../libs/glad/glad.h"
 #include <GLFW/glfw3.h>
 
-std::string readFile(const std::filesystem::path& path) {
+
+std::string read_file(const std::filesystem::path& path) {
+
     std::ifstream in(path, std::ios::binary);
     const auto sz = std::filesystem::file_size(path);
     std::string result(sz, '\0');
     in.read(result.data(), sz);
     return result;
+
 }
 
-unsigned int compileShader(unsigned int shaderType, const std::string& src) {
+
+unsigned int compile_shader(unsigned int shaderType, const std::string& src) {
+
     unsigned id = glCreateShader(shaderType);
     const char* raw = src.c_str();
     glShaderSource(id, 1, &raw, nullptr);
     glCompileShader(id);
-
-    // Проверка ошибок компиляции
-    int success;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetShaderInfoLog(id, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-
     return id;
+
 }
 
-unsigned int createShader(const std::string& vertex, const std::string& fragment) {
-    unsigned vs = compileShader(GL_VERTEX_SHADER, vertex);
-    unsigned fs = compileShader(GL_FRAGMENT_SHADER, fragment);
+
+unsigned int create_shader(const std::string& vertex, const std::string& fragment) {
+
+    unsigned vs = compile_shader(GL_VERTEX_SHADER, vertex);
+    unsigned fs = compile_shader(GL_FRAGMENT_SHADER, fragment);
 
     unsigned program = glCreateProgram();
     glAttachShader(program, vs);
     glAttachShader(program, fs);
     glLinkProgram(program);
 
-    // Проверка ошибок линковки
-    int success;
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetProgramInfoLog(program, 512, nullptr, infoLog);
-        std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-
     glValidateProgram(program);
     glDeleteShader(vs);
     glDeleteShader(fs);
 
     return program;
+
 }
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+
     glViewport(0, 0, width, height);
+
 }
 
+
 int main(void) {
+    
     GLFWwindow* window;
 
     /* Initialize the library */
@@ -67,10 +62,12 @@ int main(void) {
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(1080, 720, "Hello World", NULL, NULL);
     if (!window) {
+
         glfwTerminate();
         return -1;
+
     }
 
     /* Make the window's context current */
@@ -78,8 +75,10 @@ int main(void) {
 
     /* Init GLAD */
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
+
     }
 
     /* Create shaders */
@@ -101,26 +100,27 @@ int main(void) {
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
 
-    std::string vertex = readFile("shaders/vertex.shader");
-    std::string fragment = readFile("shaders/fragment.shader");
-    unsigned int shader = createShader(vertex, fragment);
+    std::string vertex = read_file("shaders/vertex.shader");
+    std::string fragment = read_file("shaders/fragment.shader");
+    unsigned int shader = create_shader(vertex, fragment);
     glUseProgram(shader);
 
     /* Set the viewport */
-    glViewport(0, 0, 640, 480);
+    glViewport(0, 0, 1080, 720);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Получение локации uniform-переменных
-    int resolutionLocation = glGetUniformLocation(shader, "resolution");
-    int timeLocation = glGetUniformLocation(shader, "time");
+    /* Uniforms */
+    int resolution_location = glGetUniformLocation(shader, "resolution");
+    int time_location = glGetUniformLocation(shader, "time");
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        /* Render */
-        float timeValue = static_cast<float>(glfwGetTime());
 
-        // Установка значений uniform-переменных
-        glUniform2f(resolutionLocation, 640.0f, 480.0f); // Установите ваше разрешение
-        glUniform1f(timeLocation, timeValue);
+        /* Render */
+        float time_value = static_cast<float>(glfwGetTime());
+
+        glUniform2f(resolution_location, 1080.0f, 720.0f);
+        glUniform1f(time_location, time_value);
 
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -131,9 +131,11 @@ int main(void) {
 
         /* Poll for and process events */
         glfwPollEvents();
+
     }
 
     glDeleteProgram(shader);
     glfwTerminate();
     return 0;
+    
 }
